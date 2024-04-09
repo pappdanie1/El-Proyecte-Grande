e3002e40aa21a321271cbe618f12097e2e5d4070
Papp Dániel <pappdani2@gmail.com>
2024-04-09 16:43:07 +0200

    Id added to movie model

--- El-Proyecte-Grande/El-Proyecte-Grande/Models/Movie.cs
+++ El-Proyecte-Grande/El-Proyecte-Grande/Models/Movie.cs
@@ -9,8 +9,9 @@ public class Movie
     public string Description { get; set; }
     public int DurationInSec { get; set; }
     
-    public Movie(string title, string director, IEnumerable<string> cast, string description, int durationInSec)
+    public Movie(int id, string title, string director, IEnumerable<string> cast, string description, int durationInSec)
     {
+        Id = id;
         Title = title;
         Director = director;
         Cast = cast;
