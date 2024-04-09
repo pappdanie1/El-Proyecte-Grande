7503c83412e21adb36e228f7e30cf218a0f05c9d
Papp Dániel <pappdani2@gmail.com>
2024-04-09 16:43:29 +0200

    MovieController with get, post, delete

---
+++ El-Proyecte-Grande/El-Proyecte-Grande/Controllers/MovieController.cs
@@ -0,0 +1,44 @@
+using AspCinema.Models;
+using El_Proyecte_Grande.Services;
+using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+
+namespace El_Proyecte_Grande.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class MovieController : ControllerBase
+{
+    private readonly IMovieRepository _movieRepository;
+
+    public MovieController(IMovieRepository movieRepository)
+    {
+        _movieRepository = movieRepository;
+    }
+
+    [HttpGet]
+    public IActionResult GetAll()
+    {
+        return Ok(_movieRepository.GetAll());
+    }
+
+    [HttpPost]
+    public IActionResult AddMovie(string title, string director, string cast, string description,
+        int durationInSec)
+    {
+        var casts = cast.Split(",").ToList();
+        return Ok(_movieRepository.AddMovie(new Movie(_movieRepository.GetAll().Count + 1, title, director, casts, description, durationInSec)));
+    }
+
+    [HttpDelete("{movieId}")]
+    public IActionResult DeleteMovie([FromRoute] int movieId)
+    {
+        var movie = _movieRepository.DeleteById(movieId);
+        if (movie == null)
+        {
+            return NotFound("Movie not found");
+        }
+
+        return Ok(movie);
+    }
+}
