e27c8e27634f20f5fc0c42aa51cbd2bd3f023bb4
Papp Dániel <pappdani2@gmail.com>
2024-04-10 14:11:34 +0200

    Added get by id to IMovieRepository.cs

--- El-Proyecte-Grande/El-Proyecte-Grande/Services/IMovieRepository.cs
+++ El-Proyecte-Grande/El-Proyecte-Grande/Services/IMovieRepository.cs
@@ -5,6 +5,7 @@ namespace El_Proyecte_Grande.Services;
 public interface IMovieRepository
 {
     IList<Movie> GetAll();
+    Movie GetById(int id);
     Movie AddMovie(Movie movie);
     Movie DeleteById(int id);
     Movie UpdateMovie(int id, Movie updatedMovie);
