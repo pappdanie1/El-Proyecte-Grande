467b048e326b8cdb1b14d289dbc5e573d4966945
Papp Dániel <pappdani2@gmail.com>
2024-04-10 10:45:59 +0200

    Singletons added to program

--- El-Proyecte-Grande/El-Proyecte-Grande/Program.cs
+++ El-Proyecte-Grande/El-Proyecte-Grande/Program.cs
@@ -9,6 +9,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddSingleton<IMovieRepository, MovieRepository>();
+builder.Services.AddSingleton<IMovieDbApi, MovieDbApi>();
+builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
+
 
 //add cors
 builder.Services.AddCors(options =>
