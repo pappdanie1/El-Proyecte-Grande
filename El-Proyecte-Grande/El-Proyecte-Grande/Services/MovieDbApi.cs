c6cb7c71b019c3db8918d92c20bc7a33be9ca771
andreanichter <andreamnichter@gmail.com>
2024-04-23 14:14:43 +0200

    Modify MovieDbApi class to get API key from user secrets

--- El-Proyecte-Grande/El-Proyecte-Grande/Services/MovieDbApi.cs
+++ El-Proyecte-Grande/El-Proyecte-Grande/Services/MovieDbApi.cs
@@ -5,15 +5,17 @@ namespace El_Proyecte_Grande.Services;
 public class MovieDbApi : IMovieDbApi
 {
     private readonly ILogger<MovieDbApi> _logger;
+    private readonly IConfiguration _configuration;
 
-    public MovieDbApi(ILogger<MovieDbApi> logger)
+    public MovieDbApi(ILogger<MovieDbApi> logger, IConfiguration configuration)
     {
         _logger = logger;
+        _configuration = configuration;
     }
 
     public async Task<string> GetMovies()
     {
-        var API_KEY = "81041f1501b21c327e1c6644a030c201"; //appsettings.json  //Environment.GetVariable
+        var API_KEY = _configuration["MovieDbApiKey"];
         var API_URL = $"https://api.themoviedb.org/3/movie/now_playing?api_key={API_KEY}&language=en-US&page=1";
         
         using var client = new HttpClient();
