ca5d927fd919f63ccd2bdf84cf0be09bb5ba4efc
Papp Dániel <pappdani2@gmail.com>
2024-04-24 10:44:29 +0200

    Genres added

--- El-Proyecte-Grande/El-Proyecte-Grande/Models/Movie.cs
+++ El-Proyecte-Grande/El-Proyecte-Grande/Models/Movie.cs
@@ -3,11 +3,11 @@ namespace AspCinema.Models;
 public class Movie
 {
     public int Id { get; set; }
-    public string Title { get; set; }
+    public string? Title { get; set; }
     public string? Director { get; set; }
     public string? Cast { get; set; }
-    public string Description { get; set; }
-    public int? DurationInSec { get; set; }
+    public string? Description { get; set; }
+    public string? DurationInSec { get; set; }
     public string? Poster { get; set; }
-    
+    public string? Genres { get; set; }
 }
