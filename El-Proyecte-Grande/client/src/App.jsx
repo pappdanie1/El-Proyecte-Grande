70559bf9c24464088365974dff320c54ab5aca10
andreanichter <andreamnichter@gmail.com>
2024-04-25 12:12:53 +0200

    Add screenings state

--- El-Proyecte-Grande/client/src/App.jsx
+++ El-Proyecte-Grande/client/src/App.jsx
@@ -9,13 +9,19 @@ import PageNotFound from "./Pages/PageNotFound/PageNotFound";
 function App() {
 
   const [data, setData] = useState([]);
+  const [screenings, setScreenings] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       const response = await fetch("http://localhost:5229/Movie");
       const jsonData = await response.json();
       setData(jsonData);
+
+      const screeningResponse = await fetch("http://localhost:5229/Screening");
+      const screeningJsonData = await screeningResponse.json();
+      setScreenings(screeningJsonData);
     };
+
     fetchData();
   }, []);
   console.log(data);
@@ -23,8 +29,8 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/" element={<Home data={data}/>} />
-        <Route path="movieDetails/:movieId" element={<MovieDetails data={data}/>} />
+        <Route path="/" element={<Home data={data} screenings={screenings}/>} />
+        <Route path="movieDetails/:movieId" element={<MovieDetails data={data} screenings={screenings}/>} />
         <Route path="reservation" element={<Reservation />} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
