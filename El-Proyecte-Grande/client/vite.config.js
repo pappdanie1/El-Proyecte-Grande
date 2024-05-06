b4f2deb48f57c56160884ed7a286c032fe4d0745
andreanichter <andreamnichter@gmail.com>
2024-05-06 17:05:32 +0200

    Enable server proxy

--- El-Proyecte-Grande/client/vite.config.js
+++ El-Proyecte-Grande/client/vite.config.js
@@ -3,7 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      "/api": {
+        target: 'http://localhost:5229/',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ''),
+      }
+    }
+  }
 })
+
 
 
