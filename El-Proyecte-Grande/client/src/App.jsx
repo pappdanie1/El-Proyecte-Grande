3b454b81011cd68c44e841cec36d8231b272d969
andreanichter <andreamnichter@gmail.com>
2024-05-06 17:03:04 +0200

    Add register and login routes

--- El-Proyecte-Grande/client/src/App.jsx
+++ El-Proyecte-Grande/client/src/App.jsx
@@ -5,35 +5,45 @@ import Home from "./Pages/Home";
 import MovieDetails from "./Pages/MovieDetails";
 import Reservation from "./Pages/Reservation";
 import PageNotFound from "./Pages/PageNotFound/PageNotFound";
+import RegistrationPage from "./Pages/RegistrationPage";
+import LoginPage from "./Pages/LoginPage";
+import Header from "./Components/Header";
+import Footer from "./Components/Footer";
 
 function App() {
 
   const [data, setData] = useState([]);
   const [screenings, setScreenings] = useState([]);
+  const isAuthenticated = localStorage.getItem('token') !== null;
 
   useEffect(() => {
     const fetchData = async () => {
-      const response = await fetch("http://localhost:5229/Movie");
+      const response = await fetch("/api/Movie");
+      console.log(response)
       const jsonData = await response.json();
       setData(jsonData);
 
-      const screeningResponse = await fetch("http://localhost:5229/Screening");
+      const screeningResponse = await fetch("/api/Screening");
       const screeningJsonData = await screeningResponse.json();
       setScreenings(screeningJsonData);
     };
 
     fetchData();
-  }, []);
+  }, [isAuthenticated]);
   console.log(data);
 
   return (
     <BrowserRouter>
+    <Header isAuthenticated={isAuthenticated}/>
       <Routes>
         <Route path="/" element={<Home data={data} screenings={screenings}/>} />
         <Route path="movieDetails/:movieId" element={<MovieDetails data={data} screenings={screenings}/>} />
         <Route path="reservation" element={<Reservation />} />
+        <Route path="/register" element={<RegistrationPage/>}/>
+        <Route path="/login" element={<LoginPage/>} />
         <Route path="*" element={<PageNotFound />} />
       </Routes>
+      <Footer/>
     </BrowserRouter>
   );
 }
