e6f64c114146c9ca55993f08d09fc178ac943547
andreanichter <andreamnichter@gmail.com>
2024-05-06 17:04:26 +0200

    Refactor component

--- El-Proyecte-Grande/client/src/Pages/Home.jsx
+++ El-Proyecte-Grande/client/src/Pages/Home.jsx
@@ -1,15 +1,12 @@
-import Header from "../Components/Header";
+
 import Movie from "../Components/Movie";
 import Slider from "../Components/Slider";
-import Footer from "../Components/Footer";
 
 const Home = ({data, screenings}) => {
   return (
     <>
-      <Header />
       <Slider />
       <Movie data={data} screenings={screenings}/>
-      <Footer/>
     </>
   );
 };
