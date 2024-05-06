54e5b4f806a8faf7becd2fae4ada0a4957d7e917
csabagy00 <csaba.gyo00@gmail.com>
2024-05-06 14:06:49 +0200

    Implemented Login endpoint and added User role for every Registry.

--- El-Proyecte-Grande/El-Proyecte-Grande/Controllers/AuthController.cs
+++ El-Proyecte-Grande/El-Proyecte-Grande/Controllers/AuthController.cs
@@ -1,6 +1,7 @@
 using El_Proyecte_Grande.Contracts;
 using El_Proyecte_Grande.Services.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using IConfiguration = Castle.Core.Configuration.IConfiguration;
 
 namespace El_Proyecte_Grande.Controllers;
 
@@ -10,7 +11,7 @@ public class AuthController : ControllerBase
 {
     private readonly IAuthService _authenticationService;
 
-    public AuthController(IAuthService authenticationService)
+    public AuthController(IAuthService authenticationService, IConfiguration configuration)
     {
         _authenticationService = authenticationService;
     }
@@ -23,7 +24,7 @@ public class AuthController : ControllerBase
             return BadRequest(ModelState);
         }
 
-        var result = await _authenticationService.RegisterAsync(request.Email, request.Username, request.Password);
+        var result = await _authenticationService.RegisterAsync(request.Email, request.Username, request.Password, "User");
 
         if (!result.Success)
         {
@@ -32,6 +33,25 @@ public class AuthController : ControllerBase
         }
 
         return CreatedAtAction(nameof(Register), new RegistrationResponse(result.Email, result.UserName));
+    }
+
+    [HttpPost("Login")]
+    public async Task<ActionResult<AuthResponse>> Authenticate([FromBody] AuthRequest request)
+    {
+        if (!ModelState.IsValid)
+        {
+            return BadRequest(ModelState);
+        }
+        
+        var result = await _authenticationService.LoginAsync(request.Email, request.Password);
+
+        if (!result.Success)
+        {
+            AddErrors(result);
+            return BadRequest(ModelState);
+        }
+
+        return Ok(new AuthResponse(result.Email, result.UserName, result.Token));
     }
 
     private void AddErrors(AuthResult result)
