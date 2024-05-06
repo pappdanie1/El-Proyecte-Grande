6f7b08b5d9da7d070779261571cae06f022a90dd
Papp Dániel <pappdani2@gmail.com>
2024-05-06 12:02:54 +0200

    Users Db context

---
+++ El-Proyecte-Grande/El-Proyecte-Grande/Data/UsersContext.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace El_Proyecte_Grande.Data;
+
+public class UsersContext : IdentityUserContext<IdentityUser>
+{
+    public UsersContext(DbContextOptions<UsersContext> options) : base(options)
+    {
+    }
+    
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+    }
+}
