27e9fe4d47fa73cc4278d79082a5d16090d96ba8
Papp Dániel <pappdani2@gmail.com>
2024-05-06 12:01:12 +0200

    Program cleanup + identity authentication

--- El-Proyecte-Grande/El-Proyecte-Grande/Program.cs
+++ El-Proyecte-Grande/El-Proyecte-Grande/Program.cs
@@ -1,25 +1,24 @@
+using System.Text;
 using El_Proyecte_Grande.Data;
 using El_Proyecte_Grande.Services;
+using El_Proyecte_Grande.Services.Authentication;
 using El_Proyecte_Grande.Services.DbSeed;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
-builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+AddServices();
+ConfigureSwagger();
+AddDbContext();
+AddAuthentication();
+AddIdentity();
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-builder.Services.AddScoped<IMovieRepository, MovieRepository>();
-builder.Services.AddScoped<ISeatRepository, SeatRepository>();
-builder.Services.AddSingleton<IMovieDbApi, MovieDbApi>();
-builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
-builder.Services.AddSingleton<IOmdbApi, OmdbApi>();
-builder.Services.AddScoped<ISeedScreenings, SeedScreenings>();
-builder.Services.AddScoped<ISeedSeats, SeedSeats>();
-builder.Services.AddScoped<IScreeningRepository, ScreeningRepository>();
-builder.Services.AddDbContext<ElProyecteGrandeContext>();
 
 var app = builder.Build();
 
@@ -34,8 +33,81 @@ app.UseRouting();
 
 app.UseCors(options => options.AllowAnyOrigin());
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
 
 app.Run();
+
+void AddServices()
+{
+    builder.Services.AddControllers();
+    builder.Services.AddScoped<IMovieRepository, MovieRepository>();
+    builder.Services.AddScoped<ISeatRepository, SeatRepository>();
+    builder.Services.AddSingleton<IMovieDbApi, MovieDbApi>();
+    builder.Services.AddSingleton<IJsonProcessor, JsonProcessor>();
+    builder.Services.AddSingleton<IOmdbApi, OmdbApi>();
+    builder.Services.AddScoped<ISeedScreenings, SeedScreenings>();
+    builder.Services.AddScoped<ISeedSeats, SeedSeats>();
+    builder.Services.AddScoped<IScreeningRepository, ScreeningRepository>();
+    builder.Services.AddScoped<IAuthService, AuthService>();
+}
+
+void ConfigureSwagger()
+{
+    
+}
+
+void AddDbContext()
+{
+    var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+    builder.Services.AddDbContext<ElProyecteGrandeContext>(options => 
+        options.UseSqlServer(connectionString, sqlOption =>
+        {
+            sqlOption.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+        }));
+    builder.Services.AddDbContext<UsersContext>(options =>
+        options.UseSqlServer(connectionString, sqlOption =>
+        {
+            sqlOption.EnableRetryOnFailure(5, TimeSpan.FromSeconds(10), null);
+        }));
+}
+
+void AddAuthentication()
+{
+    builder.Services
+        .AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+        .AddJwtBearer(options =>
+        {
+            options.TokenValidationParameters = new TokenValidationParameters()
+            {
+                ClockSkew = TimeSpan.Zero,
+                ValidateIssuer = true,
+                ValidateAudience = true,
+                ValidateLifetime = true,
+                ValidateIssuerSigningKey = true,
+                ValidIssuer = builder.Configuration["JwtSettings:ValidIssuer"],
+                ValidAudience = builder.Configuration["JwtSettings:ValidAudience"],
+                IssuerSigningKey = new SymmetricSecurityKey(
+                    Encoding.UTF8.GetBytes(builder.Configuration["SigningKey:IssuerSigningKey"])
+                ),
+            };
+        });
+}
+
+void AddIdentity()
+{
+    builder.Services
+        .AddIdentityCore<IdentityUser>(options =>
+        {
+            options.SignIn.RequireConfirmedAccount = false;
+            options.User.RequireUniqueEmail = true;
+            options.Password.RequireDigit = false;
+            options.Password.RequiredLength = 6;
+            options.Password.RequireNonAlphanumeric = false;
+            options.Password.RequireUppercase = false;
+            options.Password.RequireLowercase = false;
+        })
+        .AddEntityFrameworkStores<UsersContext>();
+}
