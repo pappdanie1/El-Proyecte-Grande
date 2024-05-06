23ecb8f1df3ace5c0a113ef8900fa9b9ed7003f5
andreanichter <andreamnichter@gmail.com>
2024-05-06 17:04:40 +0200

    Refactor component

--- El-Proyecte-Grande/client/src/Pages/MovieDetails.jsx
+++ El-Proyecte-Grande/client/src/Pages/MovieDetails.jsx
@@ -1,17 +1,11 @@
-import Header from "../Components/Header";
 import Slider from "../Components/Slider";
-import Footer from "../Components/Footer";
 import MovieInfo from "../Components/MovieInfo";
 
-
-const MovieDetails = ({data, screenings}) => {
- 
+const MovieDetails = ({ data, screenings }) => {
   return (
     <>
-    <Header/>
-    <Slider/>
-    <MovieInfo data={data} screenings={screenings}/>
-    <Footer/>
+      <Slider />
+      <MovieInfo data={data} screenings={screenings} />
     </>
   );
 };
