9457fc9321a84b8a2faf7c9f3761985f0c9b59b6
Papp Dániel <pappdani2@gmail.com>
2024-05-07 13:35:21 +0200

    Migrations

---
+++ El-Proyecte-Grande/El-Proyecte-Grande/Migrations/Users/20240507093206_addRoles.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace El_Proyecte_Grande.Migrations.Users
+{
+    /// <inheritdoc />
+    public partial class addRoles : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
