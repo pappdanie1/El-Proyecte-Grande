40c25ff3cb9fa3461da3449de6196061f6378c03
Papp Dániel <pappdani2@gmail.com>
2024-05-07 13:04:35 +0200

    ReservationController started

---
+++ El-Proyecte-Grande/El-Proyecte-Grande/Controllers/ReservationController.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace El_Proyecte_Grande.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class ReservationController : ControllerBase
+{
+    
+}
