b501747c1430b196a8879a4019328830853413f9
andreanichter <andreamnichter@gmail.com>
2024-05-08 12:06:30 +0200

    Create auditorium component

---
+++ El-Proyecte-Grande/client/src/Components/Auditorium.jsx
@@ -0,0 +1,70 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import Seat from "./Seat.jsx";
+import "./Component_css/Auditorium.css"
+
+function Auditorium() {
+  const [selectedSeats, setSelectedSeats] = useState([]);
+  const [seats, setSeats] = useState([]);
+
+  const { id } = useParams();
+
+  useEffect(() => {
+    const FetchAuditoriums = async () => {
+      const response = await fetch(`/api/Screening/${id}`);
+      const jsonData = await response.json();
+      setSeats(jsonData.auditorium.seats);
+    };
+
+    FetchAuditoriums();
+  }, [id]);
+
+  // Function to group seats by row
+  const groupSeatsByRow = () => {
+    const seatsByRow = {};
+    seats.forEach((seat) => {
+      const row = seat.row;
+      if (!seatsByRow[row]) {
+        seatsByRow[row] = [];
+      }
+      seatsByRow[row].push(seat);
+    });
+    return seatsByRow;
+  };
+
+  const seatsByRow = groupSeatsByRow();
+
+  //Toggling seats
+  const handleSeatSelect = (seatId) => {
+    setSelectedSeats((prevSelectedSeats) => {
+      if (prevSelectedSeats.includes(seatId)) {
+        return prevSelectedSeats.filter((id) => id !== seatId);
+      } else {
+        return [...prevSelectedSeats, seatId];
+      }
+    });
+  };
+
+  return (
+    <div className="auditorium">
+      <h2>Cinema Auditorium</h2>
+      <div className="seats">
+        {Object.keys(seatsByRow).map((row) => (
+          <div key={row} className="seat-row">
+            <div className="row-number">Row: {row} </div>
+            {seatsByRow[row].map((seat) => (
+              <Seat
+                key={seat.id}
+                number={seat.number}
+                isSelected={selectedSeats.includes(seat.id)}
+                onSelect={handleSeatSelect}
+              />
+            ))}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default Auditorium;
