e0e14ff2fd5cefb052e8845135d533f51a1a0fd3
csabagy00 <csaba.gyo00@gmail.com>
2024-05-09 15:44:00 +0200

    Added username property

--- El-Proyecte-Grande/El-Proyecte-Grande/Contracts/AuthResponse.cs
+++ El-Proyecte-Grande/El-Proyecte-Grande/Contracts/AuthResponse.cs
@@ -1,3 +1,3 @@
 namespace El_Proyecte_Grande.Contracts;
 
-public record AuthResponse(string Email, string Name, string PhoneNumber, string Token, string Role);
+public record AuthResponse(string Email, string Name,string UserName ,string PhoneNumber, string Token, string Role);
