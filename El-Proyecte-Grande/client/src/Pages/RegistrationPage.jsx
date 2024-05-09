60f830538e97253cb860ce6963da2741708af902
Papp Dániel <pappdani2@gmail.com>
2024-05-09 11:51:24 +0200

    navigate to login after register

--- El-Proyecte-Grande/client/src/Pages/RegistrationPage.jsx
+++ El-Proyecte-Grande/client/src/Pages/RegistrationPage.jsx
@@ -1,15 +1,20 @@
 import { useState } from "react"
+import { Navigate, useNavigate } from "react-router-dom";
 import RegistrationForm from "../Components/RegistrationForm";
+import './Pages_css/RegistrationPage.css'
 
 function RegistrationPage() {
     const [formData, setFormData] = useState({
         email: '',
         username: '',
+        name: '',
+        phoneNumber: '',
         password: ''
     });
 
     const [errorMessage, setErrorMessage] = useState('');
     const [registrationSuccess, setRegistrationSuccess] = useState(false);
+    const navigate = useNavigate();
 
     const handleChange = (e) => {
         const {name, value} = e.target;
@@ -35,6 +40,7 @@ function RegistrationPage() {
                 const errorData = await response.json();
                 throw new Error(errorData);
             }
+            navigate('/login')
 
             setRegistrationSuccess(true);
             console.log("Registration successful");
@@ -43,11 +49,11 @@ function RegistrationPage() {
         }
     }
     return (
-        <>
+        <div className="full-height">
         <RegistrationForm formData={formData} handleChange={handleChange} handleSubmit={handleSubmit}/>
         {errorMessage && <p>{errorMessage}</p>}
         {registrationSuccess && <p>Registration successful!</p>}
-        </>
+        </div>
     )
 }
 
