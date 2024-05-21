f337109acb09e986406f565fa709a6677e1a5a6f
csabagy00 <csaba.gyo00@gmail.com>
2024-05-21 11:23:36 +0200

    Added error handling for false response + api key in user secret

--- El-Proyecte-Grande/El-Proyecte-Grande/Services/Api/OmdbApi.cs
+++ El-Proyecte-Grande/El-Proyecte-Grande/Services/Api/OmdbApi.cs
@@ -7,31 +7,44 @@ public class OmdbApi : IOmdbApi
     private readonly ILogger<OmdbApi> _logger;
     private readonly IJsonProcessor _jsonProcessor;
     private readonly IMovieDbApi _movieDbApi;
+    private IConfiguration _config;
 
-    public OmdbApi(ILogger<OmdbApi> logger, IJsonProcessor jsonProcessor, IMovieDbApi movieDbApi)
+    public OmdbApi(ILogger<OmdbApi> logger, IJsonProcessor jsonProcessor, IMovieDbApi movieDbApi, IConfiguration config)
     {
         _logger = logger;
         _jsonProcessor = jsonProcessor;
         _movieDbApi = movieDbApi;
+        _config = config;
     }
 
     public async Task<List<string>> GetMovies()
     {
-        var API_KEY = "de5ff22f";
+        var API_KEY = _config["OmdbApiKey"];
         var moviesData = await _movieDbApi.GetMovies();
         var movies = _jsonProcessor.ProcessTitles(moviesData);
         var movieDownloadString = new List<string>();
         foreach (var title in movies)
         {
-            var API_URL = $"http://www.omdbapi.com/?apikey=de5ff22f&t={title}";
-            using var client = new HttpClient();
+            try
+            {
+                var API_URL = $"http://www.omdbapi.com/?apikey={API_KEY}&t={title}";
+                using var client = new HttpClient();
 
-            _logger.LogInformation("Calling Movie DB API with url: {API_URL}", API_URL);
-            var response = await client.GetAsync(API_URL);
-            var data = await response.Content.ReadAsStringAsync();
-            movieDownloadString.Add(data);
+                _logger.LogInformation("Calling Movie DB API with url: {API_URL}", API_URL);
+                var response = await client.GetAsync(API_URL);
+                var data = await response.Content.ReadAsStringAsync();
+
+                if (!data.Contains("Error"))
+                {
+                    movieDownloadString.Add(data);
+                }
+            }
+            catch (Exception e)
+            {
+                _logger.LogError(e.Message);
+                throw;
+            }
         }
-
         return movieDownloadString;
     }
 }
