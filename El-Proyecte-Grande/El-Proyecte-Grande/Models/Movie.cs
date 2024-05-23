33eea34e20ee41c0712bb70c3c7c98c2b574dab9
Papp Dániel <pappdani2@gmail.com>
2024-05-23 09:35:21 +0200

    model refactored for tests

--- El-Proyecte-Grande/El-Proyecte-Grande/Models/Movie.cs
+++ El-Proyecte-Grande/El-Proyecte-Grande/Models/Movie.cs
@@ -5,12 +5,12 @@ namespace AspCinema.Models;
 public class Movie
 {
     public int Id { get; set; }
-    public string? Title { get; set; }
+    public string Title { get; set; }
     public string? Director { get; set; }
     public string? Cast { get; set; }
     public string? Description { get; set; }
     public string? DurationInSec { get; set; }
     public string? Poster { get; set; }
     public string? Genres { get; set; }
-    public ICollection<Screening> Screenings { get; set; } = new List<Screening>();
+    public ICollection<Screening>? Screenings { get; set; } = new List<Screening>();
 }
